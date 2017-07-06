aeefa3b341d1ab0bcfa32dc94616e047cd3687c1
Michael N. Brown <michael@spreedly.com>
2017-07-05 23:58:26 -0400

    Removing Spreedly functions from dotfiles

--- zsh/functions/log2gist.zsh
+++
@@ -1,8 +0,0 @@
-log2gist () {
-        local log=$1
-        gist -a -p -d"$(basename $log | sed -e's/_/ /g' -e 's/\..*$//')" -f output.sh -c -s $log
-        if [ $? -eq 0 ] && confirm "Remove $log?"
-        then
-                rm $log
-        fi
-}
